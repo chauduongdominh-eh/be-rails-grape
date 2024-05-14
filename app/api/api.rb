6afc00bf8a350c68102a0b9c7c585d93c908aa98
Duong Do Minh Chau <chau.duongdominh@employmenthero.com>
2024-05-14 16:31:27 +0700

    Extract Notes API to a separate file

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,25 +1,8 @@
 # frozen_string_literal: true
 
 class API < Grape::API
-  version 'v1', using: :path
   format :json
 
-  resources :notes do
-    desc 'List user notes'
-    get do
-      Note.all
-    end
-
-    desc 'Get a specific note'
-    params do
-      requires :id, type: Integer, desc: 'Note ID'
-    end
-    route_param :id do
-      get do
-        Note.find(params[:id])
-      end
-    end
-  end
-
+  mount ::NotesAPI
   add_swagger_documentation
 end
--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-GrapeSwaggerRails.options.url = '/v1/swagger_doc.json'
+GrapeSwaggerRails.options.url = '/swagger_doc.json'
 GrapeSwaggerRails.options.app_url = 'http://localhost:3000'
