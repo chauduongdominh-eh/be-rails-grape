eb644f73ec8df0de676e7ef1b99d39ee2ef2e362
Duong Do Minh Chau <chau.duongdominh@employmenthero.com>
2024-05-14 17:22:36 +0700

    Add tags to API docs

--- app/api/notes_api.rb
+++ app/api/notes_api.rb
@@ -4,12 +4,17 @@ class NotesAPI < Grape::API
   version 'v1', using: :path
 
   namespace :notes do
-    desc 'List user notes'
+    desc 'List user notes' do
+      tags ['Notes']
+    end
     get do
       Note.all
     end
 
-    desc 'Create a new note'
+    desc 'Create a new note' do
+      tags ['Notes']
+    end
+
     post do
       Note.create!(declared(params))
     end
@@ -18,18 +23,24 @@ class NotesAPI < Grape::API
       requires :id, type: Integer, desc: 'Note ID'
     end
     route_param :id do
-      desc 'Get a specific note'
+      desc 'Get a specific note' do
+        tags ['Notes']
+      end
       get do
         Note.find(params[:id])
       end
 
-      desc 'Update a note'
+      desc 'Update a note' do
+        tags ['Notes']
+      end
       put do
         note = Note.find(params[:id])
         note.update!(declared(params))
       end
 
-      desc 'Delete a note'
+      desc 'Delete a note' do
+        tags ['Notes']
+      end
       delete do
         Note.find(params[:id]).destroy!
       end
